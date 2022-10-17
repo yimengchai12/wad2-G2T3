f44c18a223d893d4803e0c4ae09d4773b7750339
yimengchai12 <yimeng.chai.2021@scis.smu.edu.sg>
2022-10-17 18:39:23 +0800

    storage for pictures

--- views/HomePage.vue
+++ views/HomePage.vue
@@ -3,6 +3,9 @@
     <topNav></topNav>
     <sideNav></sideNav>
     <div>home</div>
+    <div class="form-group">
+        <input type="file" @change="uploadImage" class="form-control">
+    </div>
 
     <logIn></logIn>
     <registerUser></registerUser>
@@ -18,6 +21,7 @@ import registerUser from "./RegisterPage.vue"
 import sideNav from "../src/components/sideNav.vue"
 import topNav from "../src/components/topNav.vue";
 import homepageCarousel from "../src/components/homepageCarousel.vue";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
 
 export default {
     name: "homePage",  
@@ -29,5 +33,48 @@ export default {
         topNav,
         homepageCarousel
     },
+    
+    methods: {
+
+        uploadImage(e){
+            let file = e.target.files[0];
+            const storage = getStorage();
+            const storageRef = ref(storage, 'images/' + file.name);
+            
+            const metadata = {
+                contentType: 'image/jpeg/png/jpg',
+            };
+
+            const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+
+            uploadTask.on('state_changed', 
+                (snapshot) => {
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                    }
+                }, 
+                (error) => {
+                    // Handle unsuccessful uploads
+                    switch (error.code) {
+                    case 'storage/unauthorized':
+                    alert("User doesn't have permission to access the object");
+        // User doesn't have permission to access the object
+                    break;
+                }
+            }
+            );
+            console.log(e.target.files[0]);
+            
+        }
+    }
 } 
 </script>
