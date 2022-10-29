ee36748f89a266d15a898f0e46b402413b90eb68
aldensmu <115695607+aldensmu@users.noreply.github.com>
2022-10-29 20:17:37 +0800

    change profilePage to camel case

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ const router = createRouter({
             },
         { 
             path: "/profile", 
-            component: () => import("../../views/ProfilePage.vue"),
+            component: () => import("../../views/profilePage.vue"),
             meta: { requiresAuth: true, 
                 },
             },
