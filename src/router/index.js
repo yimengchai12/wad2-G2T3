3b27b2569499df1d65682a47614e4a9497401b58
yimengchai12 <yimeng.chai.2021@scis.smu.edu.sg>
2022-10-29 20:52:44 +0800

    resolve netlify

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,6 @@ const router = createRouter({
         { path: "/register", component: () => import("../components/RegisterPage.vue") },
         { path: "/sign-in", component: () => import("../components/SignIn.vue") },
         { path: "/collection", component: () => import("../../views/collectionPage.vue") },
-        { path: "/profile", component: () => import("../../views/profilePage.vue"),},
         {
             path: "/feed", 
             component: () => import("../../views/FeedPage.vue"),
@@ -23,7 +22,7 @@ const router = createRouter({
             },
         { 
             path: "/profile", 
-            component: () => import("../../views/profilePage.vue"),
+            component: () => import("../../views/ProfilePage.vue"),
             meta: { requiresAuth: true, 
                 },
             },
