35efd79a1d10c74838071017e54aadf296608416
aldensmu <115695607+aldensmu@users.noreply.github.com>
2022-10-29 23:42:27 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const router = createRouter({
         { path: "/register", component: () => import("../components/RegisterPage.vue") },
         { path: "/sign-in", component: () => import("../components/SignIn.vue") },
         { path: "/collection", component: () => import("../../views/collectionPage.vue") },
-        { path: "/profile", component: () => import("../../views/profilePage.vue"),},
+        { path: "/profile", component: () => import("../../views/ProfilePage.vue"),},
         {
             path: "/feed", 
             component: () => import("../../views/FeedPage.vue"),
