0a15d7e8911e8a2312b7e428d793239a4c9c2114
Al-Janzeer <janzeer_97@hotmail.sg>
2022-10-30 15:08:43 +0800

    added navbar chat function

--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -42,6 +42,15 @@
                     
                 </li>
                 <li class="nav-item" id="signout">
+                <div class="dropdown">
+                    <button id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" class="nav-link rounded-pill register-on-hover light-text py-1 px-3 mx-1 pl-4" v-if="isLoggedIn" >Chat</button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <!-- <li><a class="dropdown-item" href="#">Action</a></li> -->
+                        <ChatPage :currentUser="{'id': id, 'name': name, 'email': email, 'photoUrl':photoUrl}"></ChatPage>
+                    </ul>
+                </div>
+                </li>
+                <li class="nav-item" id="signout">
                     <button class="nav-link rounded-pill register-on-hover light-text py-1 px-3 mx-1 pl-4" v-if="isLoggedIn" @click="handleSignOut">Sign out</button>
                 </li>
                 <li class="nav-item">
@@ -90,8 +99,20 @@ const handleSignOut = () => {
 </script>
 
 <script>
+import ChatPage from "../components/Chat.vue"
 export default {
     name: 'topNav',
+    components:{
+        ChatPage
+    },
+    data(){
+        return{
+            id: "12345",
+            name:"Jan",
+            email:"test@gmail.com",
+            photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS6EIQlkehqQvaTOa4XoNPzIdkvIrXIgGM74dUa8Ll0A&s"
+        }
+    }
     
 }
 </script>
@@ -156,6 +177,10 @@ header a {
     text-decoration: none;
     color: #fefffe;
     transition: all 0.3s ease 0s;
+}
+
+.dropdown-menu{
+    width: max-content;
 }
 
     
