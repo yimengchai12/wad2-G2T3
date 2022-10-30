b26022985a9a3f1c3f13eac6a1cdb4d5b4169f65
aldensmu <115695607+aldensmu@users.noreply.github.com>
2022-10-30 16:18:17 +0800

    modifed sidenav

--- src/components/sideNav.vue
+++ src/components/sideNav.vue
@@ -8,8 +8,12 @@
            
                     <router-link to="/feedpage" class="nav-link text-light pl-4" v-if="isLoggedIn">Feedpage</router-link>
                 </li> -->
-                <li class="nav-item">
-                    <a class="nav-link text-light pl-4">Marketplace</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light pl-4" id="marketplace_dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Marketplace</a>
+                    <ul class="dropdown-menu dropdown_menu dropdown_menu--animated dropdown_menu-6" aria-labelledby="marketplace_dropdown">
+                        <li><router-link to="/listing" class="dropdown-item text-light pl-4 p-2">Popular Artwork</router-link></li>
+                        <li><a href="#" class="dropdown-item text-light pl-4 p-2">Upcoming Calendar</a></li>
+                    </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-light pl-4" id="creators_dropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Creators</a>
@@ -19,7 +23,7 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-light pl-4">Marketplace</a>
+                    <a class="nav-link text-light pl-4">Resources</a>
                 </li>   
                 <!-- <li class="nav-item" v-if="!isLoggedIn" id="login">
                     <a class="nav-link text-light pl-4" data-bs-toggle="modal" data-bs-target="#login" >Sign in</a>
@@ -53,8 +57,6 @@ export default {
 <style scoped>
 
 /* BELOW IS SIDENAV */
-
-
 .navbar {
     height: 100vh;
     /* margin-left: -300px; */
